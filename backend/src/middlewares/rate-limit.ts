f999036271184d84e11f2b60996bc45b4082c21a
z-u-s-h-o <zigy0ygiz@yandex.ru>
2026-07-03 21:09:53 +0500

    fix(security): add authLimiter for request customers

--- backend/src/middlewares/rate-limit.ts
+++ backend/src/middlewares/rate-limit.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit'
 
 export const authLimiter = rateLimit({
     windowMs: 1000, // 1 секунда
-    max: 2,
+    max: 5,
     message: {
         success: false,
         message: 'Слишком много запросов. Попробуйте позже.',
@@ -13,7 +13,7 @@ export const authLimiter = rateLimit({
 
 export const publicLimiter = rateLimit({
     windowMs: 1000, // 1 секунда
-    max: 3,
+    max: 10,
     message: { success: false, message: 'Превышен лимит запросов.' },
     standardHeaders: true,
     legacyHeaders: false,
--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -7,11 +7,17 @@ import {
 } from '../controllers/customers'
 import auth, { roleGuardMiddleware } from '../middlewares/auth'
 import { Role } from '../models/user'
+import { authLimiter } from '../middlewares/rate-limit'
 
 const customerRouter = Router()
 
-// customerRouter.get('/', auth, getCustomers)
-customerRouter.get('/', auth, roleGuardMiddleware(Role.Admin), getCustomers)
+customerRouter.get(
+    '/',
+    auth,
+    authLimiter,
+    roleGuardMiddleware(Role.Admin),
+    getCustomers
+)
 customerRouter.get('/:id', auth, getCustomerById)
 customerRouter.patch('/:id', auth, updateCustomer)
 customerRouter.delete('/:id', auth, deleteCustomer)
